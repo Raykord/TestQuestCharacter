0e4d583a2dc5e9370e3e2a03b6df70356e756790
Ray <96531334+Raykord@users.noreply.github.com>
2024-07-02 14:57:20 +0700

    Scene swiching

---
+++ Assets/_Project/Develop/Game/ScenesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScenesController : MonoBehaviour
+{
+    [SerializeField] private ScenesControllerView view;
+    private string _sceneName;
+
+
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject); 
+    }
+
+    public void CallLoadScene(string sceneName)
+    {
+        _sceneName = sceneName;
+        StartCoroutine(LoadScene());
+    }
+
+    private IEnumerator LoadScene()
+    {
+        yield return SceneManager.LoadSceneAsync("Loading");
+
+        view.ShowLoadingUI();
+
+        AsyncOperation loadConcreteSceen = SceneManager.LoadSceneAsync(_sceneName);
+        loadConcreteSceen.allowSceneActivation = false;
+
+        while (!loadConcreteSceen.isDone)
+        {
+            view.SetProgress(loadConcreteSceen.progress);
+            
+            if (loadConcreteSceen.progress == 0.9f) loadConcreteSceen.allowSceneActivation = true;
+
+            yield return null;
+        }
+        
+        
+    }
+}
